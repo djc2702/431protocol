969ac075ab1ae70ee11218782dead6133361c91e
Dakota Crane <djc2702@uw.edu>
2017-05-24 17:58:22 -0700

    fixed some typos

--- combined.py
+++ combined.py
@@ -1,4 +1,4 @@
-import socket, sys, os, subprocess, argparse
+import socket, sys, os, argparse
 
 def server(host, port):
     sock = socket.socket() #instantiate socket
@@ -16,7 +16,7 @@ def server(host, port):
 
 def client(host, port):
     sock = socket.socket() #instantiate the socket
-    sock.connect((host, port)) #connecto to the specified host and port
+    sock.connect((host, port)) #connect to the specified host and port
     print('Client has been assigned socket name', sock.getsockname())
     data = sock.recv(1024) #receive 1024 bytes
     text = data.decode('ascii') #decode the data
@@ -32,6 +32,6 @@ if __name__ == '__main__':
     parser.add_argument('role', choices=choices, help='Which role to play: server or client') #add the role argument
     parser.add_argument('host', help='interface the server listens at;''host the client responds to') #add the host argument
     parser.add_argument('-p', metavar='PORT', type=int, default=1060, help='TCP port (default 1060)') #add the port argument
-    args = parser.parse_args() 
+    args = parser.parse_args()
     function = choices[args.role]
     function(args.host, args.p)
